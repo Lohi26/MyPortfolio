003b27876bc87ece5cf2020a1e3b796710ba522f
Lohi26 <lohitachandru@gmail.com>
2024-07-04 08:51:18 +0530

    Final Updation for Project Section

--- indexjs.js
+++ indexjs.js
@@ -26,13 +26,48 @@ document.querySelectorAll('.nav-link').forEach(item => {
         }
     });
 });
-// Add event listener to each timeline item for click or hover effects
-document.querySelectorAll('.timeline-item').forEach(item => {
-    item.addEventListener('click', function() {
-        // Toggle active class for expanded view or use CSS transitions
-        this.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.display-left').forEach(function(element) {
+        const projectCard = element.closest('.project-card');
+        const lsElement = projectCard.querySelector('.ls');
+        const image=projectCard.querySelector('.project-img');
+        let check=projectCard.classList.contains('left-side');
+        element.addEventListener('click', function() {
+            console.log(check);
+            if (check) 
+            {
+                lsElement.classList.remove('left-side');
+                lsElement.classList.add('ls');
+                image.classList.remove('guess-img');
+            } 
+            else 
+            {
+                lsElement.classList.add('left-side');
+                lsElement.classList.remove('ls');
+                image.classList.add('guess-img');
+            }
+            check=check?false:true;
+            console.log("Hello");
+        });
     });
 });
-document.querySelector('.ls').addEventListener('click',function(){
-    
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.display-right').forEach(function(element) {
+        const projectCard = element.closest('.project-card');
+        const lsElement = projectCard.querySelector('.rs');
+        let check=projectCard.classList.contains('right-side');
+        element.addEventListener('click', function() {
+            if (check) 
+            {
+                lsElement.classList.remove('right-side');
+                lsElement.classList.add('rs');
+            } 
+            else 
+            {
+                lsElement.classList.add('right-side');
+                lsElement.classList.remove('rs');
+            }
+            check=check?false:true;
+        });
+    });
 });
