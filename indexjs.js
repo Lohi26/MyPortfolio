9b60ec139f34ea1cb26db31e91689e2b5b5de2bb
Lohi26 <lohitachandru@gmail.com>
2024-07-08 18:24:58 +0530

    A Final anf Last Updation of My Portfolio

--- indexjs.js
+++ indexjs.js
@@ -6,6 +6,21 @@ document.querySelectorAll('.nav-link').forEach(item => {
         }
     });
 });
+const contents = document.querySelectorAll('.about-me');
+    contents.forEach(content => {
+        const text = content.innerHTML.trim();
+        content.innerHTML = '';
+        let index = 0;
+        const typingSpeed = 40;
+        function type() {
+            if (index < text.length) {
+                content.insertAdjacentHTML('beforeend', text.charAt(index));
+                index++;
+                setTimeout(type, typingSpeed);
+            }
+        }
+        type();
+    });
 document.addEventListener('DOMContentLoaded',function(){
     document.querySelectorAll('.display-left').forEach(function(element) {
         const projectCard = element.closest('.project-card');
